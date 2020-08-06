ac7d75a3caa77fdd34c39b267788aee3ece430f6
abdelaty <abdelaty.mohammedmagdi@gmail.com>
2020-08-06 10:29:47 +0200

    enable hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: double.infinity,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                  Container(
+                    color: Colors.lightGreen,
+                    height: 100.0,
+                    width: 100.0,
+                    child: Text(
+                      'Android Developer',
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 10.0),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.center,
+                color: Colors.blue,
+                height: double.infinity,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
